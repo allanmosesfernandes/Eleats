d3975e7f74d4ed86ccd476837a214fb755cc8280
Allan Fernandes <fernzallan14@gmail.coms>
2020-08-17 02:51:58 +0530

    migration

---
+++ main.js
@@ -0,0 +1,18 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// const swiper = new Swiper(...);
+// var mySwiper = new Swiper('.swiper-container', {
+//     // Optional parameters
+    
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+
+//   })
